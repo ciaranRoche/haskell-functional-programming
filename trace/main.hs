41a1be3bc232662584eb8701c93f3510896e5135
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-21 12:37:32 +0000

    add tree insert

--- trace/main.hs
+++ trace/main.hs
@@ -25,3 +25,27 @@ sort ( x : xs ) = insert x (sort xs)
 insert :: (Ord a, Show a)=> a -> [a] -> [a]
 insert x [ ] = [ x ]
 insert x ( y : ys ) = if x <= y then (x:y:ys) else y : insert x ys
+
+data Tree a = 
+    EmptyTree
+    | Node (Tree a) a (Tree a) deriving (Show, Read, Eq)
+
+t :: Tree Int
+t = Node (Node EmptyTree 3 EmptyTree) 5 (Node EmptyTree 7 EmptyTree)
+
+occurs x (Node l y r)   | x == y = True
+                        | x < y = occurs x l
+                        | x > y = occurs x r
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = Node EmptyTree x EmptyTree
+treeInsert x (Node l a r)   | x == a = Node l x r
+                            | x <= a = Node (treeInsert x l) a r
+                            | x > a = Node l a (treeInsert x r)
+
+flatten :: Tree a -> [a]
+flatten EmptyTree = []
+flatten (Node EmptyTree x EmptyTree) = [x]
+flatten (Node l x r) = flatten l
+                        ++ [x]
+                        ++ flatten r
